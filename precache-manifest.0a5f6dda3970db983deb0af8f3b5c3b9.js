1f8e3698f597f60f082dd50d524c3e89642db5c8
Thanh Nguyen <nguyenhm.thanh@gmail.com>
2019-11-04 16:18:12 -0800

    Updates

---
+++ precache-manifest.0a5f6dda3970db983deb0af8f3b5c3b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5b66ba7c5f0d045e4e62e72e84e841e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f30b4821d97456400c0",
+    "url": "/static/css/2.9d6dfeae.chunk.css"
+  },
+  {
+    "revision": "d7953156b68df878933e",
+    "url": "/static/css/main.86c94d92.chunk.css"
+  },
+  {
+    "revision": "9f30b4821d97456400c0",
+    "url": "/static/js/2.00f3e1ff.chunk.js"
+  },
+  {
+    "revision": "d7953156b68df878933e",
+    "url": "/static/js/main.8d021577.chunk.js"
+  },
+  {
+    "revision": "ed4750f41f4543566353",
+    "url": "/static/js/runtime-main.fbe98da3.js"
+  },
+  {
+    "revision": "ddfab15459f4eb0b1dfb4319529839b4",
+    "url": "/static/media/no_image.ddfab154.png"
+  }
+]);
